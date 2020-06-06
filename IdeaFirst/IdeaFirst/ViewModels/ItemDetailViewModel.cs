e98cfabebd4ff926ae1fc0df9b37ef8e1b85a4d8
shixin <shixin1359@gmail.com>
2020-06-06 20:27:40 +0800

    添加项目文件。

---
+++ IdeaFirst/IdeaFirst/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using IdeaFirst.Models;
+
+namespace IdeaFirst.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
